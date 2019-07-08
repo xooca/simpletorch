e46b98d70f1c38995416cc65d35954c8f3769b4b
Prabhat <prabhat8k@gmail.com>
2019-07-08 22:54:12 +0530

    created resnet50 model

--- cv_models.py
+++ cv_models.py
@@ -10,17 +10,18 @@ class resnet50(nn.Module):
         self.pretrained = pretrained
         self.modelpath = modelpath
 
-    def create_model(self,output_class=1000, input_class=6,freezelonlylastlayer=False):
+    def create_model(self,output_class=1000, input_channel=6,freezelonlylastlayer=False):
         if self.pretrained:
             self.model  = models.resnet50(pretrained=True)
         else:
             self.model  = models.resnet50(pretrained=False)
             checkpoint = torch.load(self.modelpath)
             self.model.load_state_dict(checkpoint)
+            del checkpoint
         if freezelonlylastlayer:
             for param in self.model.parameters():
                 param.requires_grad = False
-            self.model.conv1 = nn.Conv2d(input_class, 64, 7, 2, 3)
+            self.model.conv1 = nn.Conv2d(input_channel, 64, 7, 2, 3)
             self.model.fc = nn.Sequential(
                 nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                 nn.Dropout(p=0.25),
@@ -30,7 +31,7 @@ class resnet50(nn.Module):
                 nn.Dropout(p=0.5),
                 nn.Linear(in_features=2048, out_features=output_class, bias=True) ,)
         else:
-            self.model.conv1 = nn.Conv2d(input_class, 64, 7, 2, 3)
+            self.model.conv1 = nn.Conv2d(input_channel, 64, 7, 2, 3)
             self.model.fc = nn.Sequential(
                 nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                 nn.Dropout(p=0.25),
@@ -40,6 +41,51 @@ class resnet50(nn.Module):
                 nn.Dropout(p=0.5),
                 nn.Linear(in_features=2048, out_features=output_class, bias=True) ,)
 
+    def forward(self, x):
+        out = self.model(x)
+        # out = F.relu(out, inplace=True)
+        # out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+        # out = self.model.fc(out)
+        return out
+
+
+class densenet201(nn.Module):
+    def __init__(self, pretrained=True ,modelpath=None ):
+        super().__init__()
+        self.pretrained = pretrained
+        self.modelpath = modelpath
+        # del checkpoint
+
+    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False):
+        if self.pretrained:
+            self.model = models.densenet201(pretrained=True)
+        else:
+            self.model = models.densenet201(pretrained=False)
+            checkpoint = torch.load(self.modelpath)
+            self.model.load_state_dict(checkpoint)
+            del checkpoint
+        if freezelonlylastlayer:
+            for param in self.model.parameters():
+                param.requires_grad = False
+            self.model.features.conv0 = nn.Conv2d(input_channel, 64, 7, 2, 3)
+            self.model.classifier = nn.Sequential(
+                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                nn.Dropout(p=0.25),
+                nn.Linear(in_features=1920, out_features=1920, bias=True),
+                nn.ReLU(),
+                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                nn.Dropout(p=0.5),
+                nn.Linear(in_features=1920, out_features=output_class, bias=True), )
+        else:
+            self.model.features.conv0 = nn.Conv2d(input_channel, 64, 7, 2, 3)
+            self.model.classifier = nn.Sequential(
+                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                nn.Dropout(p=0.25),
+                nn.Linear(in_features=1920, out_features=1920, bias=True),
+                nn.ReLU(),
+                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                nn.Dropout(p=0.5),
+                nn.Linear(in_features=1920, out_features=output_class, bias=True), )
     def forward(self, x):
         out = self.model(x)
         # out = F.relu(out, inplace=True)
