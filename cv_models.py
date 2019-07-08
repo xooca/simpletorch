9b5365c94e905a121de09c9f0d53fae9d54da33d
Prabhat <prabhat8k@gmail.com>
2019-07-08 23:15:55 +0530

    created resnet152 model

--- cv_models.py
+++ cv_models.py
@@ -186,3 +186,50 @@ class inceptionv3(nn.Module):
         # out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
         # out = self.model.fc(out)
         return out
+
+class resnet152(nn.Module):
+    def __init__(self, pretrained=True ,modelpath=None ):
+        super().__init__()
+        self.pretrained = pretrained
+        self.modelpath = modelpath
+        # del checkpoint
+
+    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False):
+        if self.pretrained:
+            self.model  = models.resnet152(pretrained=True)
+        else:
+            self.model  = models.resnet152(pretrained=False)
+            checkpoint = torch.load(self.modelpath)
+            self.model.load_state_dict(checkpoint)
+            del checkpoint
+        if freezelonlylastlayer:
+            for param in self.model.parameters():
+                param.requires_grad = False
+            self.model.conv1 = nn.Conv2d(input_channel, 64, 7, 2,3)
+            self.model.fc = nn.Sequential(
+                        nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                          nn.Dropout(p=0.25),
+                          nn.Linear(in_features=2048, out_features=2048, bias=True),
+                          nn.ReLU(),
+                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                          nn.Dropout(p=0.5),
+                          nn.Linear(in_features=2048, out_features=output_class, bias=True),
+                )
+        else:
+            self.model.conv1 = nn.Conv2d(input_channel, 64, 7, 2,3)
+            self.model.fc = nn.Sequential(
+                        nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                          nn.Dropout(p=0.25),
+                          nn.Linear(in_features=2048, out_features=2048, bias=True),
+                          nn.ReLU(),
+                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                          nn.Dropout(p=0.5),
+                          nn.Linear(in_features=2048, out_features=output_class, bias=True),
+                )
+
+    def forward(self, x):
+        out = self.model(x)
+        # out = F.relu(out, inplace=True)
+        # out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+        # out = self.model.fc(out)
+        return out
