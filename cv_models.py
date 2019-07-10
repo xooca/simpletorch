a8ed175d7864d48db9c323582f82a0573d42c0ee
Prabhat <prabhat8k@gmail.com>
2019-07-10 21:17:39 +0530

    modified cv_trainer

--- cv_models.py
+++ cv_models.py
@@ -56,7 +56,7 @@ class densenet201(nn.Module):
         self.modelpath = modelpath
         # del checkpoint
 
-    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False):
+    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False,last_layer=None):
         if self.pretrained:
             self.model = models.densenet201(pretrained=True)
         else:
@@ -68,24 +68,30 @@ class densenet201(nn.Module):
             for param in self.model.parameters():
                 param.requires_grad = False
             self.model.features.conv0 = nn.Conv2d(input_channel, 64, 7, 2, 3)
-            self.model.classifier = nn.Sequential(
-                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                nn.Dropout(p=0.25),
-                nn.Linear(in_features=1920, out_features=1920, bias=True),
-                nn.ReLU(),
-                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                nn.Dropout(p=0.5),
-                nn.Linear(in_features=1920, out_features=output_class, bias=True), )
+            if last_layer is None:
+                self.model.classifier = nn.Sequential(
+                    nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                    nn.Dropout(p=0.25),
+                    nn.Linear(in_features=1920, out_features=1920, bias=True),
+                    nn.ReLU(),
+                    nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                    nn.Dropout(p=0.5),
+                    nn.Linear(in_features=1920, out_features=output_class, bias=True), )
+            else:
+                self.model.classifier = last_layer
         else:
             self.model.features.conv0 = nn.Conv2d(input_channel, 64, 7, 2, 3)
-            self.model.classifier = nn.Sequential(
-                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                nn.Dropout(p=0.25),
-                nn.Linear(in_features=1920, out_features=1920, bias=True),
-                nn.ReLU(),
-                nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                nn.Dropout(p=0.5),
-                nn.Linear(in_features=1920, out_features=output_class, bias=True), )
+            if last_layer is None:
+                self.model.classifier = nn.Sequential(
+                    nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                    nn.Dropout(p=0.25),
+                    nn.Linear(in_features=1920, out_features=1920, bias=True),
+                    nn.ReLU(),
+                    nn.BatchNorm1d(1920, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                    nn.Dropout(p=0.5),
+                    nn.Linear(in_features=1920, out_features=output_class, bias=True), )
+            else:
+                self.model.classifier = last_layer
     def forward(self, x):
         out = self.model(x)
         # out = F.relu(out, inplace=True)
@@ -100,7 +106,7 @@ class densenet161(nn.Module):
         self.modelpath = modelpath
         # del checkpoint
 
-    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False):
+    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False, last_layer=None):
         if self.pretrained:
             self.model  = models.densenet161(pretrained=True)
         else:
@@ -112,26 +118,32 @@ class densenet161(nn.Module):
             for param in self.model.parameters():
                 param.requires_grad = False
             self.model.features.conv0 = nn.Conv2d(input_channel, 96, 7, 2,3)
-            self.model.classifier = nn.Sequential(
-                          nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.25),
-                          nn.Linear(in_features=2208, out_features=2208, bias=True),
-                          nn.ReLU(),
-                          nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.5),
-                          nn.Linear(in_features=2208, out_features=output_class, bias=True),
-                         )
+            if last_layer is None:
+                self.model.classifier = nn.Sequential(
+                              nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.25),
+                              nn.Linear(in_features=2208, out_features=2208, bias=True),
+                              nn.ReLU(),
+                              nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.5),
+                              nn.Linear(in_features=2208, out_features=output_class, bias=True),
+                             )
+            else:
+                self.model.classifier = last_layer
         else:
             self.model.features.conv0 = nn.Conv2d(input_channel, 96, 7, 2,3)
-            self.model.classifier = nn.Sequential(
-                          nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.25),
-                          nn.Linear(in_features=2208, out_features=2208, bias=True),
-                          nn.ReLU(),
-                          nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.5),
-                          nn.Linear(in_features=2208, out_features=output_class, bias=True),
-                         )
+            if last_layer is None:
+                self.model.classifier = nn.Sequential(
+                              nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.25),
+                              nn.Linear(in_features=2208, out_features=2208, bias=True),
+                              nn.ReLU(),
+                              nn.BatchNorm1d(2208, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.5),
+                              nn.Linear(in_features=2208, out_features=output_class, bias=True),
+                             )
+            else:
+                self.model.classifier = last_layer
 
     def forward(self, x):
         out = self.model(x)
@@ -147,7 +159,7 @@ class inceptionv3(nn.Module):
         self.modelpath = modelpath
         # del checkpoint
 
-    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False):
+    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False,last_layer=None):
         if self.pretrained:
             self.model  =models.inception_v3(pretrained=True)
         else:
@@ -159,26 +171,32 @@ class inceptionv3(nn.Module):
             for param in self.model.parameters():
                 param.requires_grad = False
             self.model.Conv2d_1a_3x3.conv = nn.Conv2d(input_channel, 32, 3, 2)
-            self.model.fc = nn.Sequential(
-                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.25),
-                          nn.Linear(in_features=2048, out_features=2048, bias=True),
-                          nn.ReLU(),
-                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.5),
-                          nn.Linear(in_features=2048, out_features=output_class, bias=True),
-                         )
+            if last_layer is None:
+                self.model.fc = nn.Sequential(
+                              nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.25),
+                              nn.Linear(in_features=2048, out_features=2048, bias=True),
+                              nn.ReLU(),
+                              nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.5),
+                              nn.Linear(in_features=2048, out_features=output_class, bias=True),
+                             )
+            else:
+                self.model.fc = last_layer
         else:
             self.model.Conv2d_1a_3x3.conv = nn.Conv2d(input_channel, 32, 3, 2)
-            self.model.fc = nn.Sequential(
-                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.25),
-                          nn.Linear(in_features=2048, out_features=2048, bias=True),
-                          nn.ReLU(),
-                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.5),
-                          nn.Linear(in_features=2048, out_features=output_class, bias=True),
-                         )
+            if last_layer is None:
+                self.model.fc = nn.Sequential(
+                              nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.25),
+                              nn.Linear(in_features=2048, out_features=2048, bias=True),
+                              nn.ReLU(),
+                              nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.5),
+                              nn.Linear(in_features=2048, out_features=output_class, bias=True),
+                             )
+            else:
+                self.model.fc = last_layer
 
     def forward(self, x):
         out = self.model(x)
@@ -194,7 +212,7 @@ class resnet152(nn.Module):
         self.modelpath = modelpath
         # del checkpoint
 
-    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False):
+    def create_model(self, output_class=1000, input_channel=6, freezelonlylastlayer=False,last_layer=None):
         if self.pretrained:
             self.model  = models.resnet152(pretrained=True)
         else:
@@ -206,26 +224,32 @@ class resnet152(nn.Module):
             for param in self.model.parameters():
                 param.requires_grad = False
             self.model.conv1 = nn.Conv2d(input_channel, 64, 7, 2,3)
-            self.model.fc = nn.Sequential(
-                        nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.25),
-                          nn.Linear(in_features=2048, out_features=2048, bias=True),
-                          nn.ReLU(),
-                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.5),
-                          nn.Linear(in_features=2048, out_features=output_class, bias=True),
-                )
+            if last_layer is None:
+                self.model.fc = nn.Sequential(
+                            nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.25),
+                              nn.Linear(in_features=2048, out_features=2048, bias=True),
+                              nn.ReLU(),
+                              nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.5),
+                              nn.Linear(in_features=2048, out_features=output_class, bias=True),
+                    )
+            else:
+                self.model.fc = last_layer
         else:
             self.model.conv1 = nn.Conv2d(input_channel, 64, 7, 2,3)
-            self.model.fc = nn.Sequential(
-                        nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.25),
-                          nn.Linear(in_features=2048, out_features=2048, bias=True),
-                          nn.ReLU(),
-                          nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                          nn.Dropout(p=0.5),
-                          nn.Linear(in_features=2048, out_features=output_class, bias=True),
-                )
+            if last_layer is None:
+                self.model.fc = nn.Sequential(
+                            nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.25),
+                              nn.Linear(in_features=2048, out_features=2048, bias=True),
+                              nn.ReLU(),
+                              nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                              nn.Dropout(p=0.5),
+                              nn.Linear(in_features=2048, out_features=output_class, bias=True),
+                    )
+            else:
+                self.model.fc = last_layer
 
     def forward(self, x):
         out = self.model(x)
